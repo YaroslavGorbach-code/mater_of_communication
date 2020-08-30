f6e5dbfc9f204c03424bacb2504520fa50ed1293
YaroslavGorbach-code <yaroslavgorbach2@gmail.com>
2020-08-30 14:14:32 +0300

    добавил отображение записей в список

--- app/src/main/java/com/YaroslavGorbach/delusionalgenerator/AudioListActivity.java
+++ app/src/main/java/com/YaroslavGorbach/delusionalgenerator/AudioListActivity.java
@@ -1,16 +1,66 @@
 package com.YaroslavGorbach.delusionalgenerator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.io.File;
 
 public class AudioListActivity extends AppCompatActivity {
 
+    private BottomSheetBehavior mBottomSheetBehavior;
+    private RecyclerView mAudioList;
+    private File[] mAllFiles;
+    private AudioListAdapter mAudioListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_list);
+        initializeComponents();
+
+        String path = this.getExternalFilesDir("/").getAbsolutePath();
+        File directory = new File(path);
+        mAllFiles = directory.listFiles();
+
+        mAudioListAdapter = new AudioListAdapter(mAllFiles, new AudioListAdapter.onItemListClick() {
+            @Override
+            public void onClickListener(File file, int position) {
+
+            }
+        });
+
+        mAudioList.setHasFixedSize(true);
+        mAudioList.setLayoutManager(new LinearLayoutManager(this));
+        mAudioList.setAdapter(mAudioListAdapter);
+
+        // фикс бага который скрывает плеер если потянуть вниз
+        mBottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if(newState == BottomSheetBehavior.STATE_HIDDEN){
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                //We cant do anything here for this app
+            }
+        });
+    }
+
+    private void initializeComponents() {
+        ConstraintLayout mPlayerSheet = findViewById(R.id.player_sheet);
+        mBottomSheetBehavior = BottomSheetBehavior.from(mPlayerSheet);
+        mAudioList = findViewById(R.id.audio_list_view);
     }
 
     private void setTheme(){
